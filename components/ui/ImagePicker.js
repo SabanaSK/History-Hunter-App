a0f4c7ae8c4830d3442bc89a32d5358f6eaf31c9
SabanaSK <sornkarn_su@hotmail.com>
2023-08-11 14:47:34 +0200

    Expo-Camera And createForm
    Install expo-camera
    uninstall expoimagepicker

---
+++ components/ui/ImagePicker.js
@@ -0,0 +1,72 @@
+import { View, Dimensions, StyleSheet, Text, Image } from "react-native"
+import { Camera, CameraType } from "expo-camera";
+import { useRef, useState } from "react";
+import IconButton from "./IconButton";
+
+const ImagePicker = () => {
+  const cameraRef = useRef();
+  const [photo, setPhoto] = useState();
+
+  const takePicture = async () => {
+    
+    if (cameraRef.current) {
+      
+      const takenPhoto = await cameraRef.current.takePictureAsync({
+        quality: 0.7,
+        exif: false,
+      });
+      console.log(takenPhoto)
+      setPhoto(takenPhoto);
+    };
+  };
+
+  let previewContent = <Text>No image taken yet</Text>
+  if (photo) {
+    previewContent = <Image source={{ uri: photo.uri }} />
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        ref={cameraRef}
+        type={CameraType.back}
+
+      >
+        <IconButton
+          icon="camera-alt"
+          size={32}
+          color="white"
+          onPress={takePicture}
+        />
+      </Camera>
+      <View style={styles.preview}></View>
+    </View>
+  )
+}
+
+const { width, height } = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    width: width,
+    height: height / 2.5,
+    alignItems: "center",/* få den i center*/
+    justifyContent: "flex-end", /* Få camera att vara längds ner */
+  },
+  preview: {
+    width: width,
+    height: height / 2,
+    backgroundColor: "blue",
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+
+  }
+
+});
+
+export default ImagePicker;
