c5df025bf56e1c595c19c7ac97e42e3a3aee0977
SabanaSK <sornkarn_su@hotmail.com>
2023-09-18 14:47:50 +0200

    fix http
    When no invite friend handle it as no friend

--- util/http.js
+++ util/http.js
@@ -96,28 +96,25 @@ export const saveHunt = async (hunt) => {
 
 export const completeHunt = async (huntId, userId) => {
   try {
-    // Fetch the current hunt data
     const res = await axios.get(`${url}/hunts/${huntId}.json`);
     const currentHunt = res.data;
     console.log("http", currentHunt);
 
-    // Update the status for the creator
     if (currentHunt.creator && currentHunt.creator.id === userId) {
       currentHunt.creator.status = "Medal";
     }
 
-    // Check if invitees exist
     if (currentHunt.invitees) {
-      // Update the status for any invitees
       currentHunt.invitees = currentHunt.invitees.map((invitee) => {
         if (invitee.id === userId) {
           return { ...invitee, status: "Medal" };
         }
         return invitee;
       });
+    } else {
+      currentHunt.invitees = [];
     }
 
-    // Update the hunt data in Firebase
     await axios.put(`${url}/hunts/${huntId}.json`, currentHunt);
   } catch (error) {
     console.error("Failed to complete the hunt", error);
